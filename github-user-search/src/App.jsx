efce6b09055231b4372dcf7f20a13f26273b7ac0
Faithy0001 <faithspenn@gmail.com>
2026-01-29 16:09:24 +0100

    Set up GitHub user search application with React and Axios

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>GitHub User Search Application</h1>
+      <p>Search for GitHub users using the search feature.</p>
+    </div>
+  );
+}
+
+export default App;

fcdc913e0e1f3319d352e47144f2ca477112b64a
Faithy0001 <faithspenn@gmail.com>
2026-02-16 19:06:18 +0100

    Add QuizSetup component with category and difficulty selection

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import TestAPI from './components/TestAPI';
+import QuizSetup from './components/QuizSetup';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/test-api" element={<TestAPI />} />
+        <Route path="/quiz-setup" element={<QuizSetup />} />
       </Routes>
     </Router>
   );

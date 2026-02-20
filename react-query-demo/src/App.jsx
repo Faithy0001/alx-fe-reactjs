6be6eae7b1d412a4ffc5c70925d2fe14183fa297
Faithy0001 <faithspenn@gmail.com>
2026-02-20 23:21:53 +0100

    Add React Query demo with data fetching and caching

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import PostsComponent from './components/PostsComponent';
+
+function App() {
+  const [showPosts, setShowPosts] = useState(true);
+
+  return (
+    <div className="App">
+      <h1>React Query Demo</h1>
+      <button onClick={() => setShowPosts(!showPosts)}>
+        {showPosts ? 'Hide Posts' : 'Show Posts'}
+      </button>
+      {showPosts && <PostsComponent />}
+    </div>
+  );
+}
+
+export default App;
